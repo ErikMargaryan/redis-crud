867b4369fa7759108cef3f9230e0e682786a845a
ErikMargaryan <erickmargarian@gmail.com>
2022-04-21 15:46:24 +0400

    change String to K (generic type)

--- src/main/java/com/understand/anothertestredis/repository/MyCrudOperations.java
+++ src/main/java/com/understand/anothertestredis/repository/MyCrudOperations.java
@@ -2,8 +2,8 @@ package com.understand.anothertestredis.repository;
 
 import java.util.List;
 
-public interface MyCrudOperations<T, ID>  {
+public interface MyCrudOperations<T, K>  {
     T save(T t);
-    T findByKey(ID key);
+    T findByKey(K key);
     List<T> findAll();
 }
