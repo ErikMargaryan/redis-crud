d23f64c3fd00a818d5fdb6d08b4c233c9d5b1896
ErikMargaryan <erickmargarian@gmail.com>
2022-04-27 16:55:16 +0400

    remove many annotation related validation and other

--- src/main/java/com/understand/anothertestredis/entities/Message.java
+++ src/main/java/com/understand/anothertestredis/entities/Message.java
@@ -1,14 +1,10 @@
 package com.understand.anothertestredis.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -16,17 +12,11 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@JsonIgnoreProperties(ignoreUnknown = true)
 public class Message implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    //username is not required to show
-//    @JsonInclude(JsonInclude.Include.NON_NULL)
-//    @NotBlank(message = "username has to been fulfilled")
-//    @NotNull(message = "username hasn't to be null")
     private String username;
-//    @NotBlank(message = "message type hasn't to be null")
     private String content;
     private String localDateTime = LocalDateTime.now().toString();
 
--- src/main/java/com/understand/anothertestredis/entities/User.java
+++ src/main/java/com/understand/anothertestredis/entities/User.java
@@ -1,13 +1,11 @@
 package com.understand.anothertestredis.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.understand.anothertestredis.entities.enums.MessageType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
-import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.util.List;
 
@@ -15,21 +13,13 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@JsonIgnoreProperties(ignoreUnknown = true)
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-//    @NotBlank(message = "username has to been fulfilled")
-//    @NotNull(message = "username hasn't to be null")
-    //    @UsernameValidation
     private String username;
-//    @NotNull(message = "message type hasn't to be null")
     private MessageType type;
-//    @Min(18)
-//    @Max(80)
     private int age;
-//    @Email
     private String email;
     private List<Message> messages;
 
