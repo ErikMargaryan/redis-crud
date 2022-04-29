4799f2c0c78db56cd98aa34a69d6c7f56b65e282
ErikMargaryan <erickmargarian@gmail.com>
2022-04-29 15:43:43 +0400

    removed @Resource annotation and made RedisTemplate<String, Object> instead of <String, [Our type]>

--- src/main/java/com/understand/anothertestredis/repository/MessageRepository.java
+++ src/main/java/com/understand/anothertestredis/repository/MessageRepository.java
@@ -1,18 +1,18 @@
 package com.understand.anothertestredis.repository;
 
 import com.understand.anothertestredis.entities.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.Resource;
 import java.util.*;
 
 @Repository
 public class MessageRepository implements MyCrudOperations<Message, String> {
     private static final String HASH_KEY_PREFIX = "MessageKey:";
 
-    @Resource(name = "message")
-    private RedisTemplate<String, Message> template;
+    @Autowired
+    private RedisTemplate<String, Object> template;
 
     public Message save(Message entity) {
         template.opsForValue().set(HASH_KEY_PREFIX + entity.getUsername() + UUID.randomUUID(), entity);
@@ -20,7 +20,7 @@ public class MessageRepository implements MyCrudOperations<Message, String> {
     }
 
     public Message findByActualKey(String key) {
-        return template.opsForValue().get(key);
+        return (Message) template.opsForValue().get(key);
     }
 
 
@@ -28,6 +28,18 @@ public class MessageRepository implements MyCrudOperations<Message, String> {
         List<Message> messageList = new ArrayList<>();
 
         Set<String> keys = template.keys(HASH_KEY_PREFIX + "*");
+        Iterator<String> it = keys.iterator();
+
+        while (it.hasNext()) {
+            messageList.add(findByActualKey(it.next()));
+        }
+        return messageList;
+    }
+
+    public List<Message> findUserAll(String username) {
+        List<Message> messageList = new ArrayList<>();
+
+        Set<String> keys = template.keys(HASH_KEY_PREFIX + username + "*");
         Iterator<String> it = keys.iterator();
 
         while (it.hasNext()) {
--- src/main/java/com/understand/anothertestredis/repository/UserRepository.java
+++ src/main/java/com/understand/anothertestredis/repository/UserRepository.java
@@ -1,18 +1,18 @@
 package com.understand.anothertestredis.repository;
 
 import com.understand.anothertestredis.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.Resource;
 import java.util.*;
 
 @Repository
 public class UserRepository implements MyCrudOperations<User, String> {
     private static final String HASH_KEY_PREFIX = "UserKey:";
 
-    @Resource(name = "user")
-    private RedisTemplate<String, User> template;
+    @Autowired
+    private RedisTemplate<String, Object> template;
 
     public User save(User entity) {
         template.opsForValue().set(HASH_KEY_PREFIX + entity.getUsername(), entity);
@@ -20,7 +20,8 @@ public class UserRepository implements MyCrudOperations<User, String> {
     }
 
     public User findByActualKey(String key) {
-        return template.opsForValue().get(key);
+//        return template.opsForValue().get(key);
+        return (User) template.opsForValue().get(key);
     }
 
     public List<User> findAll() {
