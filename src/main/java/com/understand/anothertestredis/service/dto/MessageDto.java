6ced899d91d1de40ddcc7e8b7b2fc5cc12887ae5
ErikMargaryan <erickmargarian@gmail.com>
2022-05-04 02:16:59 +0400

    add new annotation for username (in message part)

--- src/main/java/com/understand/anothertestredis/service/dto/MessageDto.java
+++ src/main/java/com/understand/anothertestredis/service/dto/MessageDto.java
@@ -2,15 +2,15 @@ package com.understand.anothertestredis.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.understand.anothertestredis.validation.annotation.UsernameExistValidation;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -20,6 +20,7 @@ public class MessageDto {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @NotBlank(message = "username has to been fulfilled")
     @NotNull(message = "username hasn't to be null")
+    @UsernameExistValidation
     private String username;
     @NotBlank(message = "message type hasn't to be null")
     private String content;
