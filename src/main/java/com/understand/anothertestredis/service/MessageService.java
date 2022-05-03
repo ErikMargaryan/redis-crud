4d39c9dcb462869c94b1b7be2f40141a4478e98d
ErikMargaryan <erickmargarian@gmail.com>
2022-05-04 02:17:29 +0400

    optimizations

--- src/main/java/com/understand/anothertestredis/service/MessageService.java
+++ src/main/java/com/understand/anothertestredis/service/MessageService.java
@@ -3,7 +3,6 @@ package com.understand.anothertestredis.service;
 import com.understand.anothertestredis.repository.MessageRepository;
 import com.understand.anothertestredis.repository.UserRepository;
 import com.understand.anothertestredis.service.dto.MessageDto;
-import com.understand.anothertestredis.service.dto.UserDto;
 import com.understand.anothertestredis.service.mapper.MapMessageEntityDtoMapper;
 import com.understand.anothertestredis.service.mapper.MapUserEntityDtoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,22 +21,14 @@ public class MessageService {
 
     private final MapMessageEntityDtoMapper mapMessage;
 
-    private final MapUserEntityDtoMapper mapUser;
-
     @Autowired
-    public MessageService(MessageRepository messageRepository, UserRepository userRepository, MapMessageEntityDtoMapper mapMessage, MapUserEntityDtoMapper mapUser) {
+    public MessageService(MessageRepository messageRepository, UserRepository userRepository, MapMessageEntityDtoMapper mapMessage) {
         this.messageRepository = messageRepository;
         this.userRepository = userRepository;
         this.mapMessage = mapMessage;
-        this.mapUser = mapUser;
     }
 
     public MessageDto save(@Valid MessageDto dto) {
-        UserDto byUsername = mapUser.entityToDto(userRepository.findByKey(dto.getUsername()));
-        List<MessageDto> messages = byUsername.getMessages();
-        messages.add(dto);
-        byUsername.setMessages(messages);
-        userRepository.save(mapUser.dtoToEntity(byUsername));
         return mapMessage.entityToDto(messageRepository.save(mapMessage.dtoToEntity(dto)));
     }
 
