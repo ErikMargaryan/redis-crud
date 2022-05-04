30a2e41ac25b0b9b53f624ccb8faffe058174bcf
ErikMargaryan <erickmargarian@gmail.com>
2022-05-04 17:03:49 +0400

    solve enum problem

--- src/main/java/com/understand/anothertestredis/entities/enums/MessageType.java
+++ src/main/java/com/understand/anothertestredis/entities/enums/MessageType.java
@@ -1,7 +1,36 @@
 package com.understand.anothertestredis.entities.enums;
 
-public enum MessageType {
-    CHAT,
-    JOIN,
-    LEAVE
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum MessageType implements Serializable {
+
+
+    CHAT("CHAT"),
+    JOIN("JOIN"),
+    LEAVE("LEAVE");
+
+    private final String name;
+
+    MessageType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @JsonCreator
+    public static MessageType fromValue(@JsonProperty("name") String name) {
+        try {
+            return valueOf(name);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
 }
